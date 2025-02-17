a2399fdd11e24f4ca51bea1cd080b44ba608d46e
KrivosheyYuriy <lonespy@yandex.ru>
2025-02-17 21:50:26 +0300

    init

---
+++ src/main/java/org/example/webb/config/JPAConfiguration.java
@@ -0,0 +1,27 @@
+package org.example.webb.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class JPAConfiguration {
+
+    private static EntityManagerFactory emf;
+
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (emf == null) {
+            try { // Укажите persistence unit
+                emf = Persistence.createEntityManagerFactory("MyPersistenceUnit");
+            } catch (Throwable ex) {
+                System.err.println("Initial EntityManagerFactory creation failed." + ex);
+                throw new ExceptionInInitializerError(ex);
+            }
+        }
+        return emf;
+    }
+
+    public static void shutdown() {
+        if (emf != null) {
+            emf.close();
+        }
+    }
+}
---
+++ src/main/java/org/example/webb/entity/User.java
@@ -0,0 +1,19 @@
+package org.example.webb.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "email")
+    private String email;
+
+    // Геттеры, сеттеры, конструкторы
+}
